9c68a0fbfe8878419038e47aaebb8283cdab14ab
Robin Appelman <robin@icewind.nl>
2022-08-08 16:05:02 +0200

    fix copyright headers
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- lib/Sabre/Album/AlbumRoot.php
+++ lib/Sabre/Album/AlbumRoot.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 /**
- * @copyright Copyright (c) 2021 Robin Appelman <robin@icewind.nl>
+ * @copyright Copyright (c) 2022 Robin Appelman <robin@icewind.nl>
  *
  * @license GNU AGPL version 3 or any later version
  *
