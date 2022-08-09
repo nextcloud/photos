14fc150a253a96ed09c016712aeb2a893a1e9363
Louis Chemineau <louis@chmn.me>
2022-08-09 16:33:25 +0200

    Use Object.freeze to make some objects read only
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- src/store/files.js
+++ src/store/files.js
@@ -58,10 +58,10 @@ const mutations = {
 			file.day = moment(file.lastmod).format('MMDD') // For On this day
 		})
 
-		state.files = {
+		state.files = Object.freeze({
 			...state.files,
 			...newFiles.reduce((files, file) => ({ ...files, [file.fileid]: file }), {}),
-		}
+		})
 	},
 
 	/**
