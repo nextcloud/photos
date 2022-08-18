611159b55cefc8f09a9d657552ba44dea615d346
Marcel Klehr <mklehr@gmx.net>
2022-08-18 18:19:27 +0200

    Explain face zooming magic
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- src/mixins/FaceCoverMixin.js
+++ src/mixins/FaceCoverMixin.js
@@ -52,6 +52,13 @@ export default {
 			  )[0]
 		},
 
+		/**
+		 * This will produce an inline style to apply to images
+		 * to zoom toward the detected face
+		 *
+		 * @param faceName
+		 * @return {{}|{transform: string, width: string, transformOrigin: string}}
+		 */
 		getCoverStyle(faceName) {
 			const cover = this.getFaceCover(faceName)
 			if (!cover) {
@@ -60,12 +67,22 @@ export default {
 			const detections = cover.faceDetections
 
 			const detection = detections.find(detection => detection.title === faceName)
+
+			// Zoom into the picture so that the face fills the --photos-face-width box nicely
+			// if the face is larger than the image, we don't zoom out (reason for the Math.max)
 			const zoom = Math.max(1, (1 / detection.width) * 0.4)
 
+			const horizontalCenterOfFace = (detection.x + detection.width / 2) * 100
+			const verticalCenterOfFace = (detection.y + detection.height / 2) * 100
+
 			return {
+				// We assume that the image is inside a div with width: var(--photos-face-width)
 				width: '100%',
-				transform: `translate(calc( var(--photos-face-width)/2 - ${(detection.x + detection.width / 2) * 100}% ), calc( var(--photos-face-width)/2 - ${(detection.y + detection.height / 2) * 100}% )) scale(${zoom})`,
-				transformOrigin: `${(detection.x + detection.width / 2) * 100}% ${(detection.y + detection.height / 2) * 100}%`,
+				// we translate the image so that the center of the detected face is in the center of the --photos-face-width box
+				// and add the zoom
+				transform: `translate(calc( var(--photos-face-width)/2 - ${horizontalCenterOfFace}% ), calc( var(--photos-face-width)/2 - ${verticalCenterOfFace}% )) scale(${zoom})`,
+				// this is necessary for the zoom to zoom toward the center of the face
+				transformOrigin: `${horizontalCenterOfFace}% ${verticalCenterOfFace}%`,
 			}
 		},
 	},
