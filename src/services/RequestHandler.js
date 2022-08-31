46512b4415e588d956cb04ac6bf58e18b891c863
Marcel Klehr <mklehr@gmx.net>
2022-08-31 15:28:10 +0200

    Move AbortController code into a vue Mixin
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- src/services/RequestHandler.js
+++
@@ -1,9 +0,0 @@
-export let abortController = new AbortController()
-
-/**
- * Cancel all running http requests
- */
-export function cancelAll() {
-	abortController.abort()
-	abortController = new AbortController()
-}
