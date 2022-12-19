21f7d86f43e9a248a419a1312f5f6e1d7fc79011
Andy Xheli <axheli@axtsolutions.com>
2022-12-19 11:04:24 -0600

    Update FaceCover.vue
    Signed-off-by: Andy Xheli <axheli@axtsolutions.com>

--- src/components/FaceCover.vue
+++ src/components/FaceCover.vue
@@ -150,12 +150,12 @@ export default {
 
 	&__crop-container {
 		overflow: hidden;
-		width: 130px;
-		height: 130px;
-		border-radius: 130px;
+		width: 128px;
+		height: 128px;
+		border-radius: 128px;
 		position: relative;
 		background: var(--color-background-darker);
-		--photos-face-width: 130px;
+		--photos-face-width: 128px;
 
 		@media only screen and (max-width: 1020px) {
 			width: 95px;
@@ -171,7 +171,7 @@ export default {
 	&__details {
 		display: flex;
 		flex-direction: column;
-		width: 130px;
+		width: 128px;
 		margin-top: 4px;
 		text-align: center;
 
