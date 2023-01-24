7da43d2ea6bec535faeb735ebd41678af3f11d09
Simon Spannagel <simonspa@kth.se>
2023-01-24 21:32:41 +0100

    Use tab as indentation
    Signed-off-by: Simon Spannagel <simonspa@kth.se>

--- lib/Listener/GroupUserRemovedListener.php
+++ lib/Listener/GroupUserRemovedListener.php
@@ -18,17 +18,17 @@ class GroupUserRemovedListener implements IEventListener {
 		if (!($event instanceof UserRemovedEvent)) {
 			return;
 		}
-        
-        // Get all shared albums for this group:
-        $albums_group = $this->albumMapper->getSharedAlbumsForCollaboratorWithFiles($event->getGroup()->getGID(), AlbumMapper::TYPE_GROUP);
-        // Get all albums shared with this specific user:
-        $albums_user = $this->albumMapper->getSharedAlbumsForCollaboratorWithFiles($event->getUser()->getUID(), AlbumMapper::TYPE_USER);
-        // Get all group-shared albums that are not directly shared with the removed user in addition
-        $albums = array_udiff($albums_group, $albums_user, fn ($a, $b) => ($a->getAlbum()->getId() - $b->getAlbum()->getId()));
 
-        // Remove their photos from theses albums:
-        foreach ($albums as $album) {
-            $this->albumMapper->removeFilesForUser($album->getAlbum()->getId(), $event->getUser()->getUID());
-        }
+		// Get all shared albums for this group:
+		$albums_group = $this->albumMapper->getSharedAlbumsForCollaboratorWithFiles($event->getGroup()->getGID(), AlbumMapper::TYPE_GROUP);
+		// Get all albums shared with this specific user:
+		$albums_user = $this->albumMapper->getSharedAlbumsForCollaboratorWithFiles($event->getUser()->getUID(), AlbumMapper::TYPE_USER);
+		// Get all group-shared albums that are not directly shared with the removed user in addition
+		$albums = array_udiff($albums_group, $albums_user, fn ($a, $b) => ($a->getAlbum()->getId() - $b->getAlbum()->getId()));
+
+		// Remove their photos from theses albums:
+		foreach ($albums as $album) {
+		    $this->albumMapper->removeFilesForUser($album->getAlbum()->getId(), $event->getUser()->getUID());
+		}
 	}
 }
