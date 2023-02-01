e66d96dc34f51fbf616a83d6431f0730d7cf1a5a
Louis Chemineau <louis@chmn.me>
2023-02-01 16:21:06 +0100

    Copy how cypress is run on the server
    Signed-off-by: Louis Chemineau <louis@chmn.me>

---
+++ cypress.config.ts
@@ -0,0 +1,89 @@
+
+import {
+	configureNextcloud,
+	startNextcloud,
+	stopNextcloud,
+	waitOnNextcloud,
+} from './cypress/dockerNode'
+import { defineConfig } from 'cypress'
+
+import browserify from '@cypress/browserify-preprocessor'
+import getCompareSnapshotsPlugin from 'cypress-visual-regression/dist/plugin'
+
+export default defineConfig({
+	projectId: 'okzqgr',
+
+	// 16/9 screen ratio
+	viewportWidth: 1280,
+	viewportHeight: 720,
+
+	// Tries again 2 more times on failure
+	retries: {
+		runMode: 2,
+		// do not retry in `cypress open`
+		openMode: 0,
+	},
+
+	// Needed to trigger `after:run` events with cypress open
+	experimentalInteractiveRunEvents: true,
+
+	// faster video processing
+	videoCompression: false,
+
+	// Visual regression testing
+	env: {
+		failSilently: false,
+		type: 'actual',
+	},
+	screenshotsFolder: 'cypress/snapshots/actual',
+	trashAssetsBeforeRuns: true,
+
+	e2e: {
+		testIsolation: false,
+
+		// We've imported your old cypress plugins here.
+		// You may want to clean this up later by importing these.
+		async setupNodeEvents(on, config) {
+			// Fix browserslist extend https://github.com/cypress-io/cypress/issues/2983#issuecomment-570616682
+			on('file:preprocessor', browserify({ typescript: require.resolve('typescript') }))
+			getCompareSnapshotsPlugin(on, config)
+
+			// Disable spell checking to prevent rendering differences
+			on('before:browser:launch', (browser, launchOptions) => {
+				if (browser.family === 'chromium' && browser.name !== 'electron') {
+					launchOptions.preferences.default['browser.enable_spellchecking'] = false
+					return launchOptions
+				}
+
+				if (browser.family === 'firefox') {
+					launchOptions.preferences['layout.spellcheckDefault'] = 0
+					return launchOptions
+				}
+
+				if (browser.name === 'electron') {
+					launchOptions.preferences.spellcheck = false
+					return launchOptions
+				}
+			})
+
+			// Remove container after run
+			on('after:run', () => {
+				stopNextcloud()
+			})
+
+			// Before the browser launches
+			// starting Nextcloud testing container
+			return startNextcloud(process.env.BRANCH)
+				.then((ip) => {
+					// Setting container's IP as base Url
+					config.baseUrl = `http://${ip}/index.php`
+					return ip
+				})
+				.then(waitOnNextcloud)
+				.then(() => configureNextcloud(process.env.BRANCH))
+				.then(() => {
+					return config
+				})
+		},
+	},
+})
---
+++ cypress/e2e/photosUtils.ts
@@ -0,0 +1,84 @@
+/**
+ * @copyright Copyright (c) 2023 Louis Chmn <louis@chmn.me>
+ *
+ * @author Louis Chmn <louis@chmn.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import type { User } from "@nextcloud/cypress"
+
+export function uploadTestMedia(user: User) {
+	cy.exec('ls cypress/fixtures/media')
+		.then((result) => {
+			for (const fileName of result.stdout.split('\n')) {
+				cy.uploadFile(user, `media/${fileName}`, 'image/png', `/${fileName}`)
+			}
+		})
+}
+
+export function selectMedia(indexes: number[]) {
+	indexes.forEach((index: number) => {
+		cy.get('[data-test="media"]').eq(index)
+			.find('a').focus()
+			.parent().find('input').check({ force: true })
+	})
+}
+
+export function unselectMedia(indexes: number[]) {
+	indexes.forEach((index: number) => {
+		cy.get('[data-test="media"]').eq(index)
+			.find('a').focus()
+			.parent().find('input').uncheck({ force: true })
+	})
+}
+
+export function favoriteSelection() {
+	cy.get('[aria-label="Open actions menu"]').click()
+	cy.get('[aria-label="Mark selection as favorite"]').click()
+}
+
+export function unfavoriteSelection() {
+	cy.get('[aria-label="Open actions menu"]').click()
+	cy.get('[aria-label="Remove selection from favorites"]').click()
+}
+
+export function downloadSelection() {
+	cy.get('[aria-label="Open actions menu"]').click()
+	cy.get('[aria-label="Download selected files"]').trigger('click')
+}
+
+export function downloadAllFiles() {
+	cy.get('[aria-label="Open actions menu"]').click()
+	cy.get('[aria-label="Download all files in album"]').trigger('click')
+}
+
+export function deleteSelection() {
+	cy.intercept({ method: 'DELETE' }).as('deleteRequests')
+	cy.get('[aria-label="Open actions menu"]').click()
+	cy.contains('Delete selection')
+		.click()
+		.wait('@deleteRequests')
+}
+
+export function goToSharedAlbum(albumName: string) {
+	cy.get('.app-navigation__list').contains('Collaborative albums').click()
+	cy.get('ul.collections__list').contains(albumName).click()
+}
+
+export function removeSharedAlbums() {
+	cy.get('[aria-label="Open actions menu"]').click()
+	cy.contains('Delete album').click()
+}
