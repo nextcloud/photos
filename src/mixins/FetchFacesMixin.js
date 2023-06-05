bf1e4cb9a323655e25b0d46e2914359cd7ff2f08
Dymirt <kolidadmytro@gmail.com>
2023-06-05 16:22:58 +0200

    Minimize data processing
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- src/mixins/FetchFacesMixin.js
+++ src/mixins/FetchFacesMixin.js
@@ -117,14 +117,19 @@ export default {
 					}
 				)
 
-				fetchedFiles = fetchedFiles
-					.map(file => genFileInfo(file))
-					.map(file => ({ ...file, filename: he.decode(file.realpath).replace(`/${getCurrentUser().uid}/files`, `/files/${getCurrentUser().uid}`) }))
-					.map(file => ({ ...file, faceDetections: JSON.parse(he.decode(file.faceDetections)) }))
-
-				const fileIds = fetchedFiles.map(file => '' + file.fileid)
-
-				this.appendFiles(fetchedFiles)
+				const processedFiles = [];
+				const fileIds = [];
+			
+				for (const file of fetchedFiles) {
+				  const processedFile = genFileInfo(file);
+				  processedFile.filename = he.decode(processedFile.realpath).replace(`/${getCurrentUser().uid}/files`, `/files/${getCurrentUser().uid}`);
+				  processedFile.faceDetections = JSON.parse(he.decode(processedFile.faceDetections));
+			
+				  processedFiles.push(processedFile);
+				  fileIds.push(String(processedFile.fileid));
+				}
+			
+				this.appendFiles(processedFiles);
 
 				if (fetchedFiles.length > 0) {
 					await this.$store.commit('addFilesToFace', { faceName, fileIdsToAdd: fileIds })
