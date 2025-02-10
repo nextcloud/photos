18bfec3ef10f7f2ba5f892b3ffccdd87248429fe
Louis Chemineau <louis@chmn.me>
2025-02-10 13:07:06 +0100

    fix: Usage of FilePicker
    Also update the lib
    
    Signed-off-by: Louis Chemineau <louis@chmn.me>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- src/components/Settings/PhotosUploadLocationSettings.vue
+++ src/components/Settings/PhotosUploadLocationSettings.vue
@@ -25,6 +25,7 @@ import { getFilePickerBuilder } from '@nextcloud/dialogs'
 import { translate as t } from '@nextcloud/l10n'
 
 import PhotosFolder from './PhotosFolder.vue'
+import logger from '../../services/logger.js'
 
 export default defineComponent({
 	name: 'PhotosUploadLocationSettings',
@@ -60,11 +61,13 @@ export default defineComponent({
 		async openFilePicker(title) {
 			const picker = getFilePickerBuilder(title)
 				.setMultiSelect(false)
-				.setModal(true)
-				.setType(1)
 				.addMimeTypeFilter('httpd/unix-directory')
 				.allowDirectories()
 				.startAt(this.photosLocation)
+				.addButton({
+					label: t('photos', 'Pick folder'),
+					callback: (nodes) => logger.debug('Picked', { nodes }),
+				})
 				.build()
 
 			return picker.pick()
