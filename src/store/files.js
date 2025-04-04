0627d8cf2bdf905d53aba03682018209a9315ab5
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2025-04-04 11:03:55 +0200

    fix(l10n): remove breaking trailing dot after curly brace
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- src/store/files.js
+++ src/store/files.js
@@ -173,8 +173,8 @@ const actions = {
 				try {
 					await client.deleteFile(file.filename)
 				} catch (error) {
-					logger.error(t('photos', 'Failed to delete {fileId}.', { fileId }), { error })
-					showError(t('photos', 'Failed to delete {fileName}.', { fileName: file.basename }))
+					logger.error(t('photos', 'Failed to delete {fileId}', { fileId }), { error })
+					showError(t('photos', 'Failed to delete {fileName}', { fileName: file.basename }))
 					console.error(error)
 					context.dispatch('appendFiles', [file])
 				} finally {
@@ -222,8 +222,8 @@ const actions = {
 					)
 				} catch (error) {
 					context.commit('favoriteFile', { fileId, favoriteState: favoriteState === 0 ? 1 : 0 })
-					logger.error(t('photos', 'Failed to set favorite state for {fileId}.', { fileId: file.fileid }), { error })
-					showError(t('photos', 'Failed to set favorite state for {fileName}.', { fileName: file.basename }))
+					logger.error(t('photos', 'Failed to set favorite state for {fileId}', { fileId: file.fileid }), { error })
+					showError(t('photos', 'Failed to set favorite state for {fileName}', { fileName: file.basename }))
 				}
 
 				return semaphore.release(symbole)
