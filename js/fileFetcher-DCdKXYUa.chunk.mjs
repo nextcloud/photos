7f887a1d6797b14844d9b7c1b4e95c83aee560cd
Louis Chemineau <louis@chmn.me>
2025-04-30 15:06:33 +0200

    chore: Compile assets
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- js/fileFetcher-DCdKXYUa.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{W as r}from"./vue.runtime.esm-C8-0a0Ou.chunk.mjs";import{W as s,_ as n}from"./index-Cqm0PfGL.chunk.mjs";async function i(a,o={}){try{const t=await s.stat(a,{data:n(),details:!0,...o});return r(t.data)}catch(t){if(t.code==="ERR_CANCELED")return null;throw t}}export{i as f};
-//# sourceMappingURL=fileFetcher-DCdKXYUa.chunk.mjs.map
