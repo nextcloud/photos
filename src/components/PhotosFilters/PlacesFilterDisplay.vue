0f41a2d9ca5345055d0f795079fef0577528d36e
Louis Chemineau <louis@chmn.me>
2025-05-16 20:36:34 +0200

    feat: Use places in the store to render places filter
    Take the opportunity to create use functions and share logic between the input and the display
    
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- src/components/PhotosFilters/PlacesFilterDisplay.vue
+++ src/components/PhotosFilters/PlacesFilterDisplay.vue
@@ -5,7 +5,7 @@
 <template>
 	<div>
 		<MapMarker /> <b>{{ t('photos', 'Places') }}: </b>
-		<span v-for="place in selectedOptions" :key="place.label">
+		<span v-for="place in selectedPlaces" :key="place.label">
 			<NcChip no-close>
 				<template #icon>
 					<img :src="place.previewUrl" class="place__preview">
@@ -18,43 +18,23 @@
 	</div>
 </template>
 <script lang="ts" setup>
-import { ref } from 'vue'
 import MapMarker from 'vue-material-design-icons/MapMarker.vue'
 
-import { generateUrl } from '@nextcloud/router'
 import { t } from '@nextcloud/l10n'
 import NcChip from '@nextcloud/vue/components/NcChip'
 
-import type { PlacesValueType } from '../../services/PhotosFilters/placesFilter.ts'
-import { fetchCollections } from '../../services/collectionFetcher.ts'
-import { placesPrefix } from '../../store/places.ts'
-
-type NcSelectPlaceOption = {
-	label: string
-	previewUrl?: string
-}
+import usePlaceFilter from './usePlaceFilter.ts'
+import type { PlacesValueType } from '../../services/PhotosFilters/placesFilter.ts';
 
 const props = defineProps<{
 	value: PlacesValueType
 }>()
 
-const loading = ref(true)
-const availableOptions = ref<NcSelectPlaceOption[]>([])
-const selectedOptions = ref<NcSelectPlaceOption[]>((props.value ?? []).map(place => ({ label: place })))
+const emit = defineEmits<{
+	(event: 'update:value', value: PlacesValueType): void
+}>()
 
-fetchCollections(placesPrefix)
-	.then(places => {
-		availableOptions.value = places.map(place => ({
-			label: place.basename,
-			previewUrl: generateUrl(`/apps/photos/api/v1/preview/${place.attributes['last-photo']}?x=${64}&y=${64}`),
-		}))
-
-		selectedOptions.value = (props.value ?? [])
-			.map(place => availableOptions.value.find(option => option.label === place))
-			.filter(place => place !== undefined)
-
-		loading.value = false
-	})
+const { selectedPlaces } = usePlaceFilter(props, emit)
 </script>
 <style lang="scss" scoped>
 .place__preview {
---
+++ src/mixins/useFetchCollections.ts
@@ -0,0 +1,49 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type { WebDAVClient } from 'webdav'
+import { ref } from 'vue'
+
+import { fetchCollections, type Collection } from '../services/collectionFetcher.js'
+import logger from '../services/logger.js'
+import store from '../store'
+import { davClient } from '../services/DavClient.ts'
+import useAbortController from './useAbortController.ts'
+
+export default function() {
+	const errorFetchingCollections = ref(null as null|number|Error|unknown)
+	const loadingCollections = ref(false)
+	const { abortSignal } = useAbortController()
+
+	async function _fetchCollections(collectionHome: string, extraProps: string[] = [], client: WebDAVClient = davClient): Promise<Collection[]> {
+		if (loadingCollections.value) {
+			return []
+		}
+
+		try {
+			loadingCollections.value = true
+			errorFetchingCollections.value = null
+
+			const collections = await fetchCollections(collectionHome, { signal: abortSignal.value }, extraProps, client)
+
+			store.dispatch('addCollections', { collections })
+
+			return collections
+		} catch (error) {
+			if (error.response?.status === 404) {
+				errorFetchingCollections.value = 404
+			} else {
+				errorFetchingCollections.value = error
+			}
+			logger.error('Error fetching collections:', { error })
+		} finally {
+			loadingCollections.value = false
+		}
+
+		return []
+	}
+
+	return { fetchCollections: _fetchCollections, errorFetchingCollections, loadingCollections }
+}
