d80187b87fe3fa0c6fd716ee4cf71dd394033075
Louis Chemineau <louis@chmn.me>
2025-05-28 11:34:53 +0200

    chore: update to ESLint v9 and adjust code
    Signed-off-by: Louis Chemineau <louis@chmn.me>

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+import { recommendedVue2 } from '@nextcloud/eslint-config'
+
+export default [
+	...recommendedVue2,
+	{
+		ignores: [
+			'js/**',
+			'l10n/**',
+		],
+		rules: {
+			'jsdoc/require-jsdoc': 'off',
+			'jsdoc/require-param': 'off',
+		},
+	},
+]
