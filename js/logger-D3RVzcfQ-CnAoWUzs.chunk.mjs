680f430aa3ae557a2b2b0a785150b7724d135b7b
Ferdinand Thiessen <opensource@fthiessen.de>
2025-06-02 11:32:34 +0200

    chore: compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ js/logger-D3RVzcfQ-CnAoWUzs.chunk.mjs
@@ -0,0 +1,2 @@
+import{E as t}from"./vue.runtime.esm-BdpTKcME.chunk.mjs";const o=t().detectUser().setApp("@nextcloud/vue").build();export{o as l};
+//# sourceMappingURL=logger-D3RVzcfQ-CnAoWUzs.chunk.mjs.map
