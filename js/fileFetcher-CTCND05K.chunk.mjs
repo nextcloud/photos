ae61b962f789812a78a6242e8d9dc4d9ad0262ba
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-08-17 03:50:10 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

--- js/fileFetcher-CTCND05K.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{I as r}from"./vue.runtime.esm-DE8Vf-Sj.chunk.mjs";import{am as s,as as n}from"./index-DiDFXBG0.chunk.mjs";async function i(a,o={}){try{const t=await s.stat(a,{data:n(),details:!0,...o});return r(t.data)}catch(t){if(t.code==="ERR_CANCELED")return null;throw t}}export{i as f};
-//# sourceMappingURL=fileFetcher-CTCND05K.chunk.mjs.map
---
+++ js/fileFetcher-DmMkpmMb.chunk.mjs
@@ -0,0 +1,2 @@
+import{I as r}from"./vue.runtime.esm-CgV0NZOD.chunk.mjs";import{am as s,as as n}from"./index-mp1XnZYt.chunk.mjs";async function i(a,o={}){try{const t=await s.stat(a,{data:n(),details:!0,...o});return r(t.data)}catch(t){if(t.code==="ERR_CANCELED")return null;throw t}}export{i as f};
+//# sourceMappingURL=fileFetcher-DmMkpmMb.chunk.mjs.map
