41b264e602ff8cdebeaa8da7e664d429c6f964ca
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-09-08 16:17:48 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/fileFetcher-7eWb2RIe.chunk.mjs
@@ -0,0 +1,2 @@
+import{r}from"./vue.runtime.esm-zA5QjVAQ.chunk.mjs";import{d as s,c}from"./index-qP_yLhAq.chunk.mjs";async function i(a,o={}){try{const t=await s.stat(a,{data:c(),details:!0,...o});return r(t.data)}catch(t){if(t.code==="ERR_CANCELED")return null;throw t}}export{i as f};
+//# sourceMappingURL=fileFetcher-7eWb2RIe.chunk.mjs.map
