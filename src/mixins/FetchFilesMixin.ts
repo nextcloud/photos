56444bc2bfaea514a1a523a9d1b3c93c1c09cebc
Louis Chemineau <louis@chmn.me>
2025-09-10 13:39:56 +0200

    fix: Mark semaphore object as raw
    Prevent vue from converting the Semaphore object to a proxy as this is breaking private property access.
    
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- src/mixins/FetchFilesMixin.ts
+++ src/mixins/FetchFilesMixin.ts
@@ -7,7 +7,7 @@ import { showError } from '@nextcloud/dialogs'
 import { defaultRootPath } from '@nextcloud/files/dav'
 import { t } from '@nextcloud/l10n'
 import { joinPaths } from '@nextcloud/paths'
-import { defineComponent } from 'vue'
+import { defineComponent, markRaw } from 'vue'
 import { davClient } from '../services/DavClient.ts'
 import logger from '../services/logger.js'
 import getPhotos, { type PhotoSearchOptions } from '../services/PhotoSearch.js'
@@ -25,7 +25,7 @@ export default defineComponent({
 			errorFetchingFiles: null as null | number | Error | unknown,
 			loadingFiles: false,
 			doneFetchingFiles: false,
-			fetchSemaphore: new SemaphoreWithPriority(1),
+			fetchSemaphore: markRaw(new SemaphoreWithPriority(1)),
 			fetchedFileIds: [] as number[],
 		}
 	},
