7d41c6ecb5ed88ef16d0b9330b190ceb21e6effb
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-09-14 03:14:56 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

---
+++ js/fileFetcher-BEshR-0z.chunk.mjs
@@ -0,0 +1,2 @@
+import{r}from"./vue.runtime.esm-CE-UW1U1.chunk.mjs";import{d as s,c}from"./index-Bx9zS1iB.chunk.mjs";async function i(a,o={}){try{const t=await s.stat(a,{data:c(),details:!0,...o});return r(t.data)}catch(t){if(t.code==="ERR_CANCELED")return null;throw t}}export{i as f};
+//# sourceMappingURL=fileFetcher-BEshR-0z.chunk.mjs.map
