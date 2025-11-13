96f7624936ac656c3ca0ba8315902c0e2730407d
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-11-13 22:04:38 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/NcVNodes-C0MrhmVc.chunk.mjs
@@ -0,0 +1,2 @@
+import{n as s}from"./index-BMeBqg0T.chunk.mjs";const t={name:"NcVNodes",props:{vnodes:{type:[Array,Object],default:null}},render(n){return this.vnodes||this.$slots?.default||this.$scopedSlots?.default?.()}},e=null,o=null;var l=s(t,e,o,!1,null,null);const a=l.exports;export{a as N};
+//# sourceMappingURL=NcVNodes-C0MrhmVc.chunk.mjs.map
