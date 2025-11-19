854c638307fbf0b1623c436b0d26195ceb51d236
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-11-19 18:08:44 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/createElementId-DhjFt1I9-B2HCdIOx.chunk.mjs
@@ -0,0 +1,2 @@
+window._nc_vue_element_id=window._nc_vue_element_id??0;function e(){return`nc-vue-${window._nc_vue_element_id++}`}export{e as c};
+//# sourceMappingURL=createElementId-DhjFt1I9-B2HCdIOx.chunk.mjs.map
