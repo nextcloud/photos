f4e90e02d9aba785252d22468675651f14a38930
Louis Chmn <louis@chmn.me>
2025-11-19 13:16:06 +0100

    chore: Compile assets
    Signed-off-by: Louis Chmn <louis@chmn.me>

---
+++ js/fileFetcher-DCpUeNCN.chunk.mjs
@@ -0,0 +1,2 @@
+import{r}from"./vue.runtime.esm-CpIjqegx.chunk.mjs";import{d as s,e as n}from"./index-RlwQk-9E.chunk.mjs";async function i(a,o={}){try{const t=await s.stat(a,{data:n(),details:!0,...o});return r(t.data)}catch(t){if(t.code==="ERR_CANCELED")return null;throw t}}export{i as f};
+//# sourceMappingURL=fileFetcher-DCpUeNCN.chunk.mjs.map
