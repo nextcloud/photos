0fd70256aafbb17d1526d3a3ddc2906a9e9ca624
Louis Chmn <louis@chmn.me>
2025-11-20 14:17:17 +0100

    feat(Settings): Use full width button for sources
    Signed-off-by: Louis Chmn <louis@chmn.me>

--- src/components/Settings/PhotosSourceLocationsSettings.vue
+++ src/components/Settings/PhotosSourceLocationsSettings.vue
@@ -4,7 +4,7 @@
 -->
 
 <template>
-	<div class="photos-locations-container">
+	<div>
 		<ul class="photos-locations">
 			<li
 				v-for="(source, index) in photosSourceFolders"
@@ -18,16 +18,15 @@
 			</li>
 		</ul>
 
-		<NcFormBox>
-			<NcFormBoxButton
-				:aria-label="t('photos', 'Add a Photos source for the timelines')"
-				@click="debounceAddSourceFolder">
-				<template #icon>
-					<Plus :size="20" />
-				</template>
-				{{ t('photos', 'Add folder') }}
-			</NcFormBoxButton>
-		</NcFormBox>
+		<NcButton
+			:aria-label="t('photos', 'Add a Photos source for the timelines')"
+			:wide="true"
+			@click="debounceAddSourceFolder">
+			<template #icon>
+				<Plus :size="20" />
+			</template>
+			{{ t('photos', 'Add folder') }}
+		</NcButton>
 	</div>
 </template>
 
@@ -36,8 +35,7 @@ import { getFilePickerBuilder } from '@nextcloud/dialogs'
 import { t } from '@nextcloud/l10n'
 import debounce from 'debounce'
 import { defineComponent } from 'vue'
-import NcFormBox from '@nextcloud/vue/components/NcFormBox'
-import NcFormBoxButton from '@nextcloud/vue/components/NcFormBoxButton'
+import NcButton from '@nextcloud/vue/components/NcButton'
 import FolderMultipleOutline from 'vue-material-design-icons/FolderMultipleOutline.vue'
 import Plus from 'vue-material-design-icons/Plus.vue'
 import PhotosFolder from './PhotosFolder.vue'
@@ -47,8 +45,7 @@ export default defineComponent({
 	name: 'PhotosSourceLocationsSettings',
 
 	components: {
-		NcFormBox,
-		NcFormBoxButton,
+		NcButton,
 		PhotosFolder,
 		Plus,
 	},
@@ -104,16 +101,11 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.photos-locations-container {
-	display: flex;
-	flex-direction: column;
+.photos-locations {
+	margin-bottom: 16px;
 
-	.photos-locations {
-		margin-bottom: 16px;
-
-		li {
-			list-style: none;
-		}
+	li {
+		list-style: none;
 	}
 }
 </style>
