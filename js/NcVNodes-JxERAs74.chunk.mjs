2ccee77872bcd673011abb443ea5d913718b6e6c
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-01-25 03:45:58 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

---
+++ js/NcVNodes-JxERAs74.chunk.mjs
@@ -0,0 +1,2 @@
+import{n as s}from"./index-DYFss36p.chunk.mjs";const t={name:"NcVNodes",props:{vnodes:{type:[Array,Object],default:null}},render(){return this.vnodes||this.$slots?.default||this.$scopedSlots?.default?.()}},e=null,o=null;var l=s(t,e,o,!1,null,null);const r=l.exports;export{r as N};
+//# sourceMappingURL=NcVNodes-JxERAs74.chunk.mjs.map
