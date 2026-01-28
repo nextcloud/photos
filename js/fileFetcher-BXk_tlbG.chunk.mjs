3b8fe834d645e321c28d8d37a738257389828c21
Ferdinand Thiessen <opensource@fthiessen.de>
2026-01-28 17:35:58 +0100

    chore: compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ js/fileFetcher-BXk_tlbG.chunk.mjs
@@ -0,0 +1,2 @@
+import{r}from"./vue.runtime.esm-BXvG6LpI.chunk.mjs";import{d as s,b as n}from"./index-DYMFnNke.chunk.mjs";async function i(a,o={}){try{const t=await s.stat(a,{data:n(),details:!0,...o});return r(t.data)}catch(t){if(t.code==="ERR_CANCELED")return null;throw t}}export{i as f};
+//# sourceMappingURL=fileFetcher-BXk_tlbG.chunk.mjs.map
