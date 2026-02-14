c92183a2b426357073ef798d1c23c3d12bea1e70
Ferdinand Thiessen <opensource@fthiessen.de>
2026-02-14 22:39:22 +0100

    chore: compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- js/createElementId-DhjFt1I9-D5Zp4M-4.chunk.mjs
+++
@@ -1,2 +0,0 @@
-var e;window._nc_vue_element_id=(e=window._nc_vue_element_id)!=null?e:0;function n(){return"nc-vue-".concat(window._nc_vue_element_id++)}export{n as c};
-//# sourceMappingURL=createElementId-DhjFt1I9-D5Zp4M-4.chunk.mjs.map
---
+++ js/dialog-BtP5MQJf.chunk.mjs
@@ -0,0 +1,2 @@
+import{V as s,t as u}from"./vue.runtime.esm-DRcchs4r.chunk.mjs";function m(i,e={},t={},o=()=>{}){typeof t=="function"&&(o=t,t={});let{container:n}=t;"container"in e&&typeof e.container=="string"&&(n??=e.container);const r=(typeof n=="string"&&document.querySelector(n)||document.body).appendChild(document.createElement("div")),c=new s({el:r,name:"VueDialogHelper",render:a=>a(i,{props:{container:null,...e},on:{close:(...p)=>{o(...p.map(l=>u(l))),c.$destroy(),r.remove()}}})});return c}export{m as s};
+//# sourceMappingURL=dialog-BtP5MQJf.chunk.mjs.map
