9e3f97ce6cae05e88bb49453aca6948fbf420643
Louis Chmn <louis@chmn.me>
2026-06-02 11:28:54 +0200

    chore: Bump @nc/vue to v8.35.0
    Signed-off-by: Louis Chmn <louis@chmn.me>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/NcIconToggleSwitch-0NTw7i-t-C0_OIodb.chunk.mjs
@@ -0,0 +1,10 @@
+import{d as a,c as t,u as i}from"./preload-helper-BKveOr2o.chunk.mjs";import{n as l,u as g}from"./index-B1ja0bGP.chunk.mjs";const p=`<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 12">
+	<path d="M17,1H7A5,5 0 0,0 2,6 5,5 0 0,0 7,11H17A5,5 0 0,0 22,6 5,5 0 0,0 17,1Z" />
+	<circle
+		cy="6"
+		r="3"
+		fill="var(--color-main-background)" />
+</svg>`,v=a({__name:"NcIconToggleSwitch",props:{checked:{type:Boolean},size:{default:34},inline:{type:Boolean,default:!1}},setup(o){const c=o;i((w,s)=>({"2d1b8583":s.color,"78386a55":s.cx}));const e=t(()=>c.checked?"var(--color-primary-element)":"var(--color-text-maxcontrast)"),n=t(()=>c.checked?"calc(17 / 24 * 100%)":"calc(7 / 24 * 100%)");return{__sfc:!0,svg:p,props:c,color:e,cx:n,NcIconSvgWrapper:g}}}),f="_iconToggleSwitch_ZSKWf",h={"material-design-icon":"_material-design-icon_YB2B5",iconToggleSwitch:f};var m=function(){var o=this,c=o._self._c,e=o._self._setupProxy;return c(e.NcIconSvgWrapper,{class:o.$style.iconToggleSwitch,attrs:{svg:e.svg,size:o.size,inline:o.inline}})},u=[];const r={$style:h};function _(o){for(var c in r)this[c]=r[c]}var d=l(v,m,u,!1,_,null);const S=d.exports;export{S as N};
+//# sourceMappingURL=NcIconToggleSwitch-0NTw7i-t-C0_OIodb.chunk.mjs.map
