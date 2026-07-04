a730740bdbcbc574fb73fb3c9286fd2b4d6297fb
Julian Lehrhuber <jl@lerry.de>
2026-07-04 20:33:20 +0200

    fix: handling of directory names with special characters
    Signed-off-by: Julian Lehrhuber <jl@lerry.de>

--- js/index-BfOqt2on.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{l as i}from"./preload-helper-szYwPwxh.chunk.mjs";function o(){try{return i("core","capabilities")}catch{return console.debug("Could not find capabilities initial state fall back to _oc_capabilities"),"_oc_capabilities"in window?window._oc_capabilities:{}}}export{o as g};
-//# sourceMappingURL=index-BfOqt2on.chunk.mjs.map
