92f2fe033fe2f60af06b3a1404eaad6e1655cbfc
Ferdinand Thiessen <opensource@fthiessen.de>
2026-07-17 18:19:26 +0200

    chore: compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ js/GenRandomId-F5ebeBB_-DI-yuh7g.chunk.mjs
@@ -0,0 +1,2 @@
+function o(t){return Math.random().toString(36).replace(/[^a-z]+/g,"").slice(0,5)}export{o as G};
+//# sourceMappingURL=GenRandomId-F5ebeBB_-DI-yuh7g.chunk.mjs.map
--- js/fileFetcher-CoQWFzHw.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{r}from"./preload-helper-iHri5ttz.chunk.mjs";import{d as s,b as n}from"./index-DXEYydlG.chunk.mjs";async function e(a,o={}){try{const t=await s.stat(a,{data:n(),details:!0,...o});return r(t.data)}catch(t){if(t.code==="ERR_CANCELED")return null;throw t}}export{e as f};
-//# sourceMappingURL=fileFetcher-CoQWFzHw.chunk.mjs.map
