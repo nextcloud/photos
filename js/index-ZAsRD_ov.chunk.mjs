fd4b475370c191af6b8fdf254f86ada05db32109
Ferdinand Thiessen <opensource@fthiessen.de>
2026-07-17 16:29:57 +0200

    chore: compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- js/index-ZAsRD_ov.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{l as i}from"./preload-helper-DiEJ7Aun.chunk.mjs";function o(){try{return i("core","capabilities")}catch{return console.debug("Could not find capabilities initial state fall back to _oc_capabilities"),"_oc_capabilities"in window?window._oc_capabilities:{}}}export{o as g};
-//# sourceMappingURL=index-ZAsRD_ov.chunk.mjs.map
