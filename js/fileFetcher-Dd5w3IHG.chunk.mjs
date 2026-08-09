8415ad9d0998e6661fb5e0ea2bf37e9c0dcef7ff
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-08-09 04:37:46 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

---
+++ js/fileFetcher-Dd5w3IHG.chunk.mjs
@@ -0,0 +1,2 @@
+import{r}from"./preload-helper-DgRUHZYY.chunk.mjs";import{d as s,b as n}from"./index-BtlMq09h.chunk.mjs";async function e(a,o={}){try{const t=await s.stat(a,{data:n(),details:!0,...o});return r(t.data)}catch(t){if(t.code==="ERR_CANCELED")return null;throw t}}export{e as f};
+//# sourceMappingURL=fileFetcher-Dd5w3IHG.chunk.mjs.map
