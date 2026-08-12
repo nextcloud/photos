c0cd4fd506a1c5215d0354d05b5aa4480e95de81
xhon-pelushi <xhon@pelushi.com>
2026-08-12 01:33:23 -0400

    fix(timeline): hide create-album label text on mobile to stop header overflow
    The Create new album from filters button kept its long label on narrow
    viewports while sibling header actions already hide theirs, which forced
    horizontal scrolling. Match the same mobile-only label visibility.
    
    Fixes #3579
    Signed-off-by: xhon-pelushi <xhon@pelushi.com>

--- src/views/TimelineView.vue
+++ src/views/TimelineView.vue
@@ -36,7 +36,9 @@
 					:aria-label="createAlbumButtonLabel"
 					data-cy-header-action="create-album"
 					@click="showAlbumCreationForm = true">
-					{{ createAlbumButtonLabel }}
+					<template v-if="!isMobile" #default>
+						{{ createAlbumButtonLabel }}
+					</template>
 					<template #icon>
 						<PlusBoxMultipleOutline />
 					</template>
