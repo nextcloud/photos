788884e404810e06322ca0a0d49bb6d31252dfc5
Ferdinand Thiessen <opensource@fthiessen.de>
2026-08-15 02:23:03 +0200

    chore: compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ js/NcActionSeparator-BgsJ3Bzd.chunk.mjs
@@ -0,0 +1,2 @@
+import{n as o}from"./index-9ze8jB6n.chunk.mjs";const r={name:"NcActionSeparator"};var s=function(){var t=this,a=t._self._c;return a("li",{staticClass:"action action-separator action--disabled",attrs:{role:"separator"}})},n=[],e=o(r,s,n,!1,null,"3e2324b7");const i=e.exports;export{i as N};
+//# sourceMappingURL=NcActionSeparator-BgsJ3Bzd.chunk.mjs.map
---
+++ js/index-BR3ZhpOz.chunk.mjs
@@ -0,0 +1,2 @@
+import{l as i}from"./dav-DzX2Mm_F.chunk.mjs";function o(){try{return i("core","capabilities")}catch{return console.debug("Could not find capabilities initial state fall back to _oc_capabilities"),"_oc_capabilities"in window?window._oc_capabilities:{}}}export{o as g};
+//# sourceMappingURL=index-BR3ZhpOz.chunk.mjs.map
