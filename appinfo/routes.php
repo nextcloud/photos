933df3f7010f5c5397e0818a78bdd6a648e3cda7
Josh <josh.t.richards@gmail.com>
2026-08-16 16:22:27 -0400

    style(routes): organize and normalize route definitions
    Normalize route formatting and trailing commas,
    and clarify the route sections/grouping with comments.
    
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -7,85 +7,158 @@
 
 return [
 	'routes' => [
-		['name' => 'api#setUserConfig', 'url' => '/api/v1/config/{key}', 'verb' => 'PUT'],
-		['name' => 'api#serviceWorker', 'url' => '/service-worker.js', 'verb' => 'GET'],
+		// API endpoints used by the Photos frontend
+		[
+			'name' => 'api#setUserConfig',
+			'url' => '/api/v1/config/{key}',
+			'verb' => 'PUT',
+		],
+		[
+			'name' => 'api#serviceWorker',
+			'url' => '/service-worker.js',
+			'verb' => 'GET',
+		],
 
-		['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
-		['name' => 'page#index', 'url' => '/thisday', 'verb' => 'GET', 'postfix' => 'thisday'],
-		['name' => 'page#index', 'url' => '/photos', 'verb' => 'GET', 'postfix' => 'photos'],
-		['name' => 'page#index', 'url' => '/videos', 'verb' => 'GET', 'postfix' => 'videos'],
-		['name' => 'page#index', 'url' => '/favorites', 'verb' => 'GET', 'postfix' => 'favorites'],
-		['name' => 'page#index', 'url' => '/albums/{path}', 'verb' => 'GET', 'postfix' => 'albums',
+		// Frontend entry points
+		[
+			'name' => 'page#index',
+			'url' => '/',
+			'verb' => 'GET',
+		],
+		[
+			'name' => 'page#index',
+			'url' => '/thisday',
+			'verb' => 'GET',
+			'postfix' => 'thisday',
+		],
+		[
+			'name' => 'page#index',
+			'url' => '/photos',
+			'verb' => 'GET',
+			'postfix' => 'photos',
+		],
+		[
+			'name' => 'page#index',
+			'url' => '/videos',
+			'verb' => 'GET',
+			'postfix' => 'videos',
+		],
+		[
+			'name' => 'page#index',
+			'url' => '/favorites',
+			'verb' => 'GET',
+			'postfix' => 'favorites',
+		],
+
+		// Collection and folder views
+		[
+			'name' => 'page#index',
+			'url' => '/albums/{path}',
+			'verb' => 'GET',
+			'postfix' => 'albums',
 			'requirements' => [
 				'path' => '.*',
 			],
 			'defaults' => [
 				'path' => '',
-			]
+			],
 		],
-		['name' => 'page#index', 'url' => '/sharedalbums/{path}', 'verb' => 'GET', 'postfix' => 'sharedalbums',
+		[
+			'name' => 'page#index',
+			'url' => '/sharedalbums/{path}',
+			'verb' => 'GET',
+			'postfix' => 'sharedalbums',
 			'requirements' => [
 				'path' => '.*',
 			],
 			'defaults' => [
 				'path' => '',
-			]
+			],
 		],
-		['name' => 'page#index', 'url' => '/places/{path}', 'verb' => 'GET', 'postfix' => 'places',
+		[
+			'name' => 'page#index',
+			'url' => '/places/{path}',
+			'verb' => 'GET',
+			'postfix' => 'places',
 			'requirements' => [
 				'path' => '.*',
 			],
 			'defaults' => [
 				'path' => '',
-			]
+			],
 		],
-		[ 'name' => 'publicAlbum#get', 'url' => '/public/{token}', 'verb' => 'GET',
+		[
+			'name' => 'page#index',
+			'url' => '/folders/{path}',
+			'verb' => 'GET',
+			'postfix' => 'folders',
+			'requirements' => [
+				'path' => '.*',
+			],
+			'defaults' => [
+				'path' => '',
+			],
+		],
+		[
+			'name' => 'page#index',
+			'url' => '/faces/{path}',
+			'verb' => 'GET',
+			'postfix' => 'faces',
+			'requirements' => [
+				'path' => '.*',
+			],
+			'defaults' => [
+				'path' => '',
+			],
+		],
+		[
+			'name' => 'page#index',
+			'url' => '/shared/{path}',
+			'verb' => 'GET',
+			'postfix' => 'shared',
+			'requirements' => [
+				'path' => '.*',
+			],
+			'defaults' => [
+				'path' => '',
+			],
+		],
+		[
+			'name' => 'page#index',
+			'url' => '/tags/{path}',
+			'verb' => 'GET',
+			'postfix' => 'tags',
+			'requirements' => [
+				'path' => '.*',
+			],
+			'defaults' => [
+				'path' => '',
+			],
+		],
+		[
+			'name' => 'page#index',
+			'url' => '/categories/{path}',
+			'verb' => 'GET',
+			'postfix' => 'categories',
+			'requirements' => [
+				'path' => '.*',
+			],
+			'defaults' => [
+				'path' => '',
+			],
+		],
+
+		// Public album view
+		[
+			'name' => 'publicAlbum#get',
+			'url' => '/public/{token}',
+			'verb' => 'GET',
 			'requirements' => [
 				'token' => '.*',
 			],
 		],
-		['name' => 'page#index', 'url' => '/folders/{path}', 'verb' => 'GET', 'postfix' => 'folders',
-			'requirements' => [
-				'path' => '.*',
-			],
-			'defaults' => [
-				'path' => '',
-			]
-		],
-		['name' => 'page#index', 'url' => '/faces/{path}', 'verb' => 'GET', 'postfix' => 'faces',
-			'requirements' => [
-				'path' => '.*',
-			],
-			'defaults' => [
-				'path' => '',
-			]
-		],
-		['name' => 'page#index', 'url' => '/shared/{path}', 'verb' => 'GET', 'postfix' => 'shared',
-			'requirements' => [
-				'path' => '.*',
-			],
-			'defaults' => [
-				'path' => '',
-			]
-		],
-		['name' => 'page#index', 'url' => '/tags/{path}', 'verb' => 'GET', 'postfix' => 'tags',
-			'requirements' => [
-				'path' => '.*',
-			],
-			'defaults' => [
-				'path' => '',
-			]
-		],
-		['name' => 'page#index', 'url' => '/categories/{path}', 'verb' => 'GET', 'postfix' => 'categories',
-			'requirements' => [
-				'path' => '.*',
-			],
-			'defaults' => [
-				'path' => '',
-			]
-		],
 
-		// apis
+		// Data APIs
 		[
 			'name' => 'albums#myAlbums',
 			'url' => '/api/v1/albums/{path}',
@@ -109,22 +182,22 @@ return [
 			],
 		],
 
+		// Preview APIs
 		[
 			'name' => 'preview#index',
 			'url' => '/api/v1/preview/{fileId}',
 			'verb' => 'GET',
 			'requirements' => [
 				'fileId' => '.*',
-			]
+			],
 		],
-
 		[
 			'name' => 'publicPreview#index',
 			'url' => '/api/v1/publicPreview/{fileId}',
 			'verb' => 'GET',
 			'requirements' => [
 				'fileId' => '.*',
-			]
+			],
 		],
-	]
+	],
 ];
