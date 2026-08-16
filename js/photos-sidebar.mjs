89a233ecb930ca178051bd981b0b2036f74aba40
Ferdinand Thiessen <opensource@fthiessen.de>
2026-08-16 16:02:41 +0200

    chore: compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- js/photos-sidebar.mjs
+++ js/photos-sidebar.mjs
@@ -1,2 +1,2 @@
-import{t as o,V as a,a as p,r as t}from"./dav-DzX2Mm_F.chunk.mjs";a.prototype.t=o,a.prototype.n=p,t("nc:metadata-photos-exif"),t("nc:metadata-photos-ifd0"),t("nc:metadata-photos-gps"),t("nc:metadata-photos-place");
+import{t as o,V as a,a as p,r as t}from"./dav-WB4rs2Oc.chunk.mjs";a.prototype.t=o,a.prototype.n=p,t("nc:metadata-photos-exif"),t("nc:metadata-photos-ifd0"),t("nc:metadata-photos-gps"),t("nc:metadata-photos-place");
 //# sourceMappingURL=photos-sidebar.mjs.map
