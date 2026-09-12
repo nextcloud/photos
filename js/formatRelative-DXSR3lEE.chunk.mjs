f33f76bd41ae4ab80657855354c73e19ee8470e1
skjnldsv <skjnldsv@protonmail.com>
2026-09-12 04:16:39 +0200

    chore(assets): compile
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

---
+++ js/formatRelative-DXSR3lEE.chunk.mjs
@@ -0,0 +1,2 @@
+const o={lastWeek:"eeee 'dernier à' p",yesterday:"'hier à' p",today:"'aujourd’hui à' p",tomorrow:"'demain à' p'",nextWeek:"eeee 'prochain à' p",other:"P"},a=(e,r,t,p)=>o[e];export{a as f};
+//# sourceMappingURL=formatRelative-DXSR3lEE.chunk.mjs.map
--- js/photos-sidebar.mjs
+++ js/photos-sidebar.mjs
@@ -1,2 +1,2 @@
-import{t as o,V as a,a as p,r as t}from"./dav-YbDjeNoy.chunk.mjs";a.prototype.t=o,a.prototype.n=p,t("nc:metadata-photos-exif"),t("nc:metadata-photos-ifd0"),t("nc:metadata-photos-gps"),t("nc:metadata-photos-place");
+import{r as t}from"./dav--9zNVXiK.chunk.mjs";import"./index-D2NRa5x8.chunk.mjs";import"./icons-BQaqLn_a.chunk.mjs";t("nc:metadata-photos-exif"),t("nc:metadata-photos-ifd0"),t("nc:metadata-photos-gps"),t("nc:metadata-photos-place");
 //# sourceMappingURL=photos-sidebar.mjs.map
