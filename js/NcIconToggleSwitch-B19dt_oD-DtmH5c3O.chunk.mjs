d8d95d5636533b6fa7b58a955a75b26503a43871
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-09-13 13:49:06 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/NcIconToggleSwitch-B19dt_oD-DtmH5c3O.chunk.mjs
@@ -0,0 +1,10 @@
+import{a,c as s,u as i}from"./preload-helper-BFCWXCIl.chunk.mjs";import{n as l,q as g}from"./index-DIZotaEq.chunk.mjs";const p=`<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 12">
+	<path d="M17,1H7A5,5 0 0,0 2,6 5,5 0 0,0 7,11H17A5,5 0 0,0 22,6 5,5 0 0,0 17,1Z" />
+	<circle
+		cy="6"
+		r="3"
+		fill="var(--color-main-background)" />
+</svg>`,v=a({__name:"NcIconToggleSwitch",props:{checked:{type:Boolean},size:{default:34},inline:{type:Boolean,default:!1}},setup(c){const o=c;i((w,t)=>({"2d1b8583":t.color,"78386a55":t.cx}));const e=s(()=>o.checked?"var(--color-primary-element)":"var(--color-text-maxcontrast)"),n=s(()=>o.checked?"calc(17 / 24 * 100%)":"calc(7 / 24 * 100%)");return{__sfc:!0,svg:p,props:o,color:e,cx:n,NcIconSvgWrapper:g}}}),h="_iconToggleSwitch_NggOU",m={"material-design-icon":"_material-design-icon_Tdewt",iconToggleSwitch:h};var _=function(){var c=this,o=c._self._c,e=c._self._setupProxy;return o(e.NcIconSvgWrapper,{class:c.$style.iconToggleSwitch,attrs:{svg:e.svg,size:c.size,inline:c.inline}})},d=[];const r={$style:m};function f(c){for(var o in r)this[o]=r[o]}var u=l(v,_,d,!1,f,null);const S=u.exports;export{S as N};
+//# sourceMappingURL=NcIconToggleSwitch-B19dt_oD-DtmH5c3O.chunk.mjs.map
