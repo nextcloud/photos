12781207629b66e25626ccd6cc9074662a379ebe
Ferdinand Thiessen <opensource@fthiessen.de>
2026-09-13 18:36:08 +0200

    test: resolve server logs in tests
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -16,8 +16,8 @@ if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
 
-require_once __DIR__ . '/../../../lib/base.php';
-require_once __DIR__ . '/../../../tests/autoload.php';
+require_once __DIR__ . '/../../../../lib/base.php';
+require_once __DIR__ . '/../../../../tests/autoload.php';
 
 if (!class_exists(TestCase::class)) {
 	require_once('PHPUnit/Autoload.php');
